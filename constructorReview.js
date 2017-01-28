0ee6af5b4735d0ff43319df7d3573c3874e3fa84
Joseph Pimentel <I59307@xactware.com>
2017-01-28 14:02:45 -0700

    Constructor review partially done

--- constructorReview.js
+++ constructorReview.js
@@ -1,34 +1,50 @@
 //Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
 
   //code here
-
+function Animal(species, name, legs, color, foodArr) {
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.food = foodArr;
+}
 
 //Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
 
   //code here
-
+function person(name, age, height, gender) {
+  return {
+    name: name,
+    age: age,
+    height: height,
+    gender: gender
+  };
+}
 
 //Create a animal array and a person array.
 
   //code here
-
+var animalArr = [];
+var personArr = [];
 
 //Create two instances of Animal and push those into your animal array
 
   //code here
-
+animalArr.push(new Animal('Tiger', 'Hobbes', 4, 'Orange', ['tuna', 'pb&j sandwhiches']))
+animalArr.push(new Animal('Bear', 'Pooh', 4, 'Yellow', ['honey', 'more honey']))
 
 //Create two instances of person and push those into your person array.
 
   //code here
-
+personArr.push(person('Calvin', 6, '3 feet 7 inches', 'male'))
+personArr.push(person('Christopher Robin', 7, '4 feet', 'male'))
 
 //Now we want every instance of Animal to have a eat method. This method will choose a random item in that instances food array, then alert "(name) ' ate ' (whichever food was chosen)".
 
   //code here
 
 
-//At this point, if we wanted to add something to every istance of person could we? 
+//At this point, if we wanted to add something to every istance of person could we?
 
   //Yes or no? and why or why not?
 
